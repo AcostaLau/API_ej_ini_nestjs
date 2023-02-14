ce3173d17b15cfe17c369eb480005bedd5d0c6db
Lautaro Acosta <soylautaroacosta@gmail.com>
2023-02-14 15:16:47 -0300

    deleted

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CategoriesController } from './controllers/categories/categories.controller';
-import { OrderController } from './controllers/order/order.controller';
 import { UserController } from './controllers/user/user.controller';
-import { CustomerController } from './controllers/customer/customer.controller';
 import { ProductController } from './controllers/product/product.controller';
-import { BrandController } from './controllers/brand/brand.controller';
 import { ProductsService } from './services/products/products.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, CategoriesController, OrderController, UserController, CustomerController, ProductController, BrandController],
+  controllers: [AppController, UserController, ProductController,],
   providers: [AppService, ProductsService],
 })
 export class AppModule { }
